af0ce93c53d13703e4ba77086bf49904fcef9d4b
Richard Myatt <lydgate77@hotmail.co.uk>
2017-12-02 21:24:27 +0000

    initial commit

---
+++ js/test_api.js
@@ -0,0 +1,27 @@
+// get a reference to the results para
+var results = document.querySelector('.results');
+
+var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=Te';
+var responseObj;
+
+// create a request object
+var request = new XMLHttpRequest();
+
+function getData() {
+  request.open('GET', url);
+  request.response = 'json';
+  request.send();
+
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        responseObj = JSON.parse(request.response);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+    results.textContent = responseObj;
+  };
+}
+
+window.onload = getData();
