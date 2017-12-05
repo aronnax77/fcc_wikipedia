e96062fae52087aaec5d7013efe44a4176a80be8
Richard Myatt <lydgate77@hotmail.co.uk>
2017-12-05 17:56:09 +0000

    page now populating with search results - further work needed in connection with random page results

--- js/viewer.js
+++ js/viewer.js
@@ -1,69 +1,103 @@
-var url = "assets/data.json";
-var responseObj;
+var url, responseObj, request;
 
-var request = new XMLHttpRequest();
 
-function getData() {
+var TESTING = false;
+var btn = document.querySelector('button');
+var chb = document.querySelector('#random');
+var sch = document.querySelector('#search');
+var res = document.querySelector('.result');
+btn.addEventListener('click', search);
+
+res.textContent = 'lot and lot';
+
+function search() {
+  //alert('in search');
+  //request = new XMLHttpRequest();
+  if(TESTING === true){
+    url = "assets/data.json";
+    alert(chb.value);
+    res.textContent = 'not working now';
+  } else if(chb.value === 'on') {
+    alert(chb.value);
+    alert('in if else');
+    res.textContent = 'It works';
+  } else {
+    //alert('in else');
+    url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=' + sch.value;
+    //alert(url);
+    //alert(request);
+  }
+//alert(request);
+//alert(url);
+getData(url);
+//getAlert();
+}
+
+function getAlert() {
+  alert('in getAlert');
+  alert(request);
+}
+
+
+function getData(source) {
+  request = new XMLHttpRequest();
   request.open('GET', url);
   request.response = 'json';
-  request.send();
-
 
   request.onreadystatechange = function() {
     if (request.readyState === 4) {
       if (request.status === 200) {
         responseObj = JSON.parse(request.response);
-        out();
+        //alert(responseObj);
+        renderArticles();
         //writePara(responseObj);
       } else {
         alert('There was a problem with the request.');
       }
     }
-    //console.log(responseObj);
   };
+
+  request.send();
+
 }
+//renderArticles(responseObj);
+//getData();
 
-getData();
+function renderArticles() {
+  var section = document.querySelector('section');
 
-//var el = document.querySelector('.out');
-//alert(el);
+  while(section.firstChild) {
+    section.removeChild(section.firstChild);
+  }
 
-/*function writePara(arg) {
-  var arr = JSON.parse(arg);
-  alert('In para');
-  alert(typeof arg);
-  alert(arr[1][2]);
-  el.textContent = arr[1][2];
-}*/
+  for(var i = 0; i < 10; i++) {
+    addArticle(i);
+  }
 
-function out() {
-  var obj = JSON.parse(responseObj);
-  var article = document.createElement('article');
-  var sect = document.querySelector('section');
-  var heading = document.createElement('h2');
-  var para    = document.createElement('p');
-  heading.textContent = obj[1][2];
-  para.textContent    = obj[2][2];
-  article.appendChild(heading);
-  article.appendChild(para);
-  sect.appendChild(article);
-  sect.addEventListener('click', openWindow);
-  sect.url = "https://en.wikipedia.org/wiki/Television_program";
+  function addArticle(itemNo) {
+    //res.textContent = JSON.parse(responseObj);
+      //var obj = responseObj;
+      //res.textContent = obj[1][1];
+      var section = document.querySelector('section');
+      var article = document.createElement('article');
+      var heading = document.createElement('h2');
+      var para    = document.createElement('p');
 
-  function openWindow(evt) {
-    alert('In open window');
-    alert(evt.target);
-    window.open(sect.url, "_blank");
+      heading.textContent = responseObj[1][itemNo];
+      para.textContent    = responseObj[2][itemNo];
+      article.appendChild(heading);
+      article.appendChild(para);
+      section.appendChild(article);
+
+      article.addEventListener('click', openWindow);
+      article.url = responseObj[3][itemNo];
+      //alert('article.url = ' + article.url);
+
+      function openWindow(evt) {
+        //alert('In open window');
+        //alert(evt.target);
+        //alert(evt.target.url);
+        window.open(this.url, "_blank");
+      }
   }
 }
-
-
-/*var search = document.querySelector('#first');
-search.addEventListener('click', openWindow);
-search.url = "https://en.wikipedia.org/wiki/Television_program";
-
-function openWindow(evt) {
-  alert('In open window');
-  alert(evt.target);
-  window.open(evt.target.url, "_blank");
-}*/
